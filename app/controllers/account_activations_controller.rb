9265af97935d5c73a51ec55a7c51ab386e0bcc6a
Gustavo <lolpsxvipkry2@gmail.com>
2016-09-03 19:01:27 +0000

    Consertando URL mais uma vez

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,26 +2,26 @@ class AccountActivationsController < ApplicationController
 
   def edit
     # emailFix porq o email tava chegando com um "3D" na frente
-    emailFix = params[:email]
-    emailFix.reverse!
-    emailFix.chop!
-    emailFix.chop!
-    emailFix.reverse!
-    user = User.find_by(email: emailFix)
+    # emailFix = params[:email]
+    # emailFix.reverse!
+    # emailFix.chop!
+    # emailFix.chop!
+    # emailFix.reverse!
+    user = User.find_by(email: params[:email])
     
-    idFix = params[:id]
-    aux = idFix.reverse.byteslice(0,2)
-    idFix.chop!
-    idFix.chop!
-    idFix.chop!
-    idFix.chop!
-    aux.reverse!
-    idFix << aux
+    #idFix = params[:id]
+    #aux = idFix.reverse.byteslice(0,2)
+    #idFix.chop!
+    #idFix.chop!
+    #idFix.chop!
+    #idFix.chop!
+    #aux.reverse!
+    #idFix << aux
     
-    #if !user.authenticated?(params[:id])
-    #  redirect_to emailFix_url
-    #  return;
-    #end
+    if !user.authenticated?(params[:id])
+      redirect_to emailFix_url
+      return;
+    end
     if user && !user.activated? && user.authenticated?(params[:id])
       user.activate
       #log_in user
