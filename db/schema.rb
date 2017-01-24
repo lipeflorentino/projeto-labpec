7671de9bbdf8a423f4bc678c2c5ec3896f61568d
Gustavo <lolpsxvipkry2@gmail.com>
2017-01-24 16:03:25 +0000

    Table Comprimissos droped

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113235329) do
+ActiveRecord::Schema.define(version: 20170124160018) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
@@ -25,14 +25,6 @@ ActiveRecord::Schema.define(version: 20170113235329) do
     t.datetime "updated_at",                   null: false
     t.index ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
     t.index ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
-  end
-
-  create_table "compromissos", force: :cascade do |t|
-    t.string   "titulo"
-    t.text     "texto"
-    t.date     "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "documentos", force: :cascade do |t|
